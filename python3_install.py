cb7d2990fb953950b46b38ab4af569e404ca5bd1
Myki <sjlh>
2018-09-26 10:48:13 +0800

    python

---
+++ python3_install.py
@@ -0,0 +1,11 @@
+#下载安装包
+wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tar.xz
+#创建文件夹
+mkdir /usr/local/python3 
+tar -xvJf  Python-3.7.0.tar.xz
+cd Python-3.7.0
+./configure --prefix=/usr/local/python3
+make && make install
+
+ln -s /usr/local/python3/bin/python3 /usr/bin/python3
+ln -s /usr/local/python3/bin/pip3 /usr/bin/pip3
