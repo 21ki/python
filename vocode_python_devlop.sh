0094d7f77d9772887c0f5ac91be46b8aa920161c
sjlh_pc <myki@sjlh.com>
2019-11-28 16:13:15 +0800

    vscode

---
+++ vocode_python_devlop.sh
@@ -0,0 +1,15 @@
+#python3.6
+cmd
+import this
+pip list
+pip install flake8
+pip install yapf
+
+
+#工作区域
+{
+    "python.linting.flake8Enabled": true,
+    "python.formatting.provider": "yapf",
+    "python.linting.flake8Args": ["--max-line-length=248"],
+    "python.linting.pylintEnabled": false
+}
